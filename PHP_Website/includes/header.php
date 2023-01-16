be67afca6b0365a020016f26f9885ce86e2d1975
Jake JR <1453194@stu.tmc.ac.uk>
2023-01-16 12:13:32 +0000

    Adding header.php

---
+++ PHP_Website/includes/header.php
@@ -0,0 +1,19 @@
+<?php
+
+$page_name = $page_name ?? '';
+
+?>
+
+<html lang="en">
+    <head>
+        <title>ToKa Fitness - <?= $page_name ?></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="static/CSS/main.css">
+        <script deter src="static/JS/navbar.js"></script>
+    </head>
+    <?php
+    include 'includes/navbar.php';
+    ?>
+</html>
