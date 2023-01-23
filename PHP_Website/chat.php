bf0a8c58bb285a3fa494688a6a1b93922b790446
Jake JR <1453194@stu.tmc.ac.uk>
2023-01-23 12:17:08 +0000

    Changing chat.php

--- PHP_Website/chat.php
+++ PHP_Website/chat.php
@@ -10,20 +10,41 @@
     </head>
 
     <body>
-        <div class="chat">
+        <?php
+
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+
+            $logged_in = $_SESSION["logged_in"] ?? false;
+            $user = $_SESSION["user"] ?? null;
+
+            if ($logged_in == true) {
+                // Checks if the security cookie is set
+                if (isset($_COOKIE["security"])) {
+                    // Checks if the cookie is valid
+                    if ($_COOKIE["security"] == $user["private_key"]) {
+                        return;
+                    } else {
+                        // If the cookie is invalid, the user is logged out for security purposes
+                        session_destroy();
+                        header("Location: login.php");
+                        exit("You have been logged out for security reasons. Please login and try again.");
+                    }
+                } else {
+                    // Creates a new security cookie that lasts an hour
+                    setcookie("security", $user["private_key"], time() + 3600, "/");
+                }
+
+            // TODO: Fix the chat not showing up when the user is logged in
+        ?>
+
+        <div id="chat" class="chat">
             <div class="chat-container">
                 <p class="chat-title">
                     Chat
                 </p>
                 <div class="chat-message-container">
-                    <div class="chat-message">
-                        <p class="author">
-                            Author
-                        </p>
-                        <p class="message">
-                            Hello, World!
-                        </p>
-                    </div>
                 </div>
                 <div class="chat-input">
                     <input type="text" placeholder="Type a message...">
@@ -32,5 +53,6 @@
             <div class="chat-icon">
             </div>
         </div>
+        <?php } ?>
     </body>
 </html>
