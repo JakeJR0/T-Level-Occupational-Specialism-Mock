644c7e6d1c17b0ceb3dbd1627d3d9910d99c333e
Jake JR <1453194@stu.tmc.ac.uk>
2023-01-23 16:59:32 +0000

    Ensuring that the security cookie gets deleted

--- PHP_Website/logout.php
+++ PHP_Website/logout.php
@@ -4,7 +4,13 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+// Destroy the session
 session_destroy();
+// Removes the security cookie
+setcookie("security", "", time() - 3600, "/");
+
+
+
 header("Location: index.php");
 
 ?>
