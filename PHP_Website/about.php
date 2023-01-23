cbf7517076fc7138ca0e438a8ff603df3a368ad0
Jake JR <1453194@stu.tmc.ac.uk>
2023-01-23 12:17:47 +0000

    Updating about.php

--- PHP_Website/about.php
+++ PHP_Website/about.php
@@ -5,6 +5,15 @@
     include 'includes/header.php';
     ?>
     <body>
+        <div class="page-title">
+            <h1>ToKa Fitness</h1>
+            <p class="description">Here at ToKa Fitness we have gyms which will suit the needs of anyone who would want to use our facilities, our commitment is to ensure that our customers gain the best customer experience possible.</p>
+        </div>
 
+        <!-- YouTube Video -->
+        <div class="video-container">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/" title="Welcome to ToKa Fitness" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
     </body>
 </html>
