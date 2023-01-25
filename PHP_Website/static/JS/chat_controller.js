923c1a518f613599f25be0bf92b073e23a86af53
Jake JR <1453194@stu.tmc.ac.uk>
2023-01-25 12:32:19 +0000

    Adding chat_controller.js

--- PHP_Website/static/JS/chat_controller.js
+++ PHP_Website/static/JS/chat_controller.js
@@ -37,35 +37,14 @@ function addMessageToChat(chat, username, message) {
     messageContainer.scrollTop = messageContainer.scrollHeight;
 }
 
-function sendMessage(chat, message="") {
-    valid = true;
-
-    if (message.length <= 0) {
-        valid = false;
-    } else if (message.length > 255) {
-        valid = false;
-    }
-
-    if (valid != true) {
-        return false;
-    }
 
 
-    var token = getSecurityAuth();
-
-    // Sends message via socket
-    socket.emit('message', {
-        'message': message,
-        'token': token
-    });
-
-}
-
-function setupChatBox(chat) {
+function setupChatBox() {
+    var chat = document.getElementById('chat');
     setup = true;
     if (chat) {
         var chatIcon = document.querySelector('.chat-icon');
-        var chatWindow = document.querySelector('.chat-container');
+        var chatWindow = document.querySelector('.user-chat-container');
         var messageContainer = chat.querySelector(".chat-message-container");
         messageContainer.scrollTop = messageContainer.scrollHeight;
 
@@ -121,50 +100,6 @@ function setupChatBox(chat) {
     return setup;
 }
 
-function setupChat() {
-    var chat = document.getElementById('chat');
-
-    if (!chat) {
-        console.log("Chat HTML was not found, disabling chat...");
-        return false;
-    }
-
-    var chatID = chat.getAttribute('data-chat-id');
-    if (!chatID || chatID == null) {
-        socket.emit('create_chat', {
-            'token': getSecurityAuth()
-        });
-
-        console.log(socket.emit());
-
-        console.log("Chat ID was not found, creating new chat...")
-    } else {
-        console.log("Chat ID found, connecting to chat...");
-    }
-
-    setup = setupChatBox(chat);
-    console.log(setup);
-    if (setup) {
-        socket.on("connect", function() {
-            console.log(socket);
-            socket.on("join", function(data) {
-                console.log("Joined chat");
-                console.log(data);
-            })
-
-
-            socket.emit('join_chat', {
-                'token': getSecurityAuth()
-            });
-
-            socket.on("message", function(data) {
-                console.log("Client ID: " + socket.id);
-                console.log(data);
-            })
-        })
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    setupChat();
+document.addEventListener('DOMContentLoaded', function() {
+    setupChatBox();
 });
