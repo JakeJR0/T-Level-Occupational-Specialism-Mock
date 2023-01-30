e6c6a49e3de88def0242a79301a524be511d5368
Jake JR <1453194@stu.tmc.ac.uk>
2023-01-30 16:52:11 +0000

    Updating index.php

--- PHP_Website/index.php
+++ PHP_Website/index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php
-    $page_name = "Home";
-    include 'includes/header.php';
-    ?>
-    <body>
-        <div class="img-text-container auto">
-            <img alt="A user exercising on a treadmill" src="static/images/home_page_main_display.jpg">
-            <div class="banner">
-                <h1>Get a Gym Membership with Online Advise, to help you maximize the effect of the gym</h1>
-                <a class="btn" href="signup.php">
-                    Join Now
-                </a>
-            </div>
+<?php
+$page_name = "Home";
+include 'includes/header.php';
+?>
+
+<body>
+    <div class="img-text-container auto">
+        <img alt="A user exercising on a treadmill" src="static/images/home_page_main_display.jpg">
+        <div class="banner">
+            <h1 aria-label="Get a Gym Membership">Get a Gym Membership with Online Advise, to help you maximize the effect of the gym</h1>
+            <a aria-label="Join Now" class="btn" href="signup.php">
+                Join Now
+            </a>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
