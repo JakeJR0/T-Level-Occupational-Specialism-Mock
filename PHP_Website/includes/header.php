b2cada5902dbdbc144109b64715c917fb5c3382f
Jake JR <1453194@stu.tmc.ac.uk>
2023-02-01 11:48:12 +0000

    Changing header.php

--- PHP_Website/includes/header.php
+++ PHP_Website/includes/header.php
@@ -9,16 +9,18 @@ $main_image = "static/images/home_page_main_display.jpg";
 ?>
 
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="static/CSS/main.css">
-        <script deter src="static/JS/navbar.js"></script>
-        <title>ToKa Fitness - <?= $page_name ?></title>
 
-    </head>
-    <?php
-    include 'includes/navbar.php';
-    ?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="static/CSS/main.css">
+    <script defer src="static/JS/navbar.js"></script>
+    <title>ToKa Fitness - <?= $page_name ?></title>
+
+</head>
+<?php
+include 'includes/navbar.php';
+?>
+
 </html>
